da82f2b6f7408f9fbd7ac9c9291984d978cee40d
Reispfannenfresser <Reispfannenfresser@no-reply.users.github.com>
2019-11-06 13:22:04 +0100

    wiki example gui fixed

--- wiki/pam_gui_example/lua/pam/client/cl_example_menu.lua
+++ wiki/pam_gui_example/lua/pam/client/cl_example_menu.lua
@@ -1,39 +1,55 @@
 local menu = {}
+-- locally stores the panel for easier access
 local panel = nil
+
+-- unique identifier for this menu
 menu.id = "Example"
 
+-- called after the client has received all important information
 function menu.OnVoteStarted()
 	panel = vgui.Create("pam_votescreen_example")
 end
 
+-- called after the vote was canceled
 function menu.OnVoteCanceled()
 	panel:Remove()
 end
 
+-- called when a player votes
+-- ply - the voting player
+-- map_id - the id of the map the player voted for
 function menu.OnVoterAdded(ply, map_id)
 	panel:AddVoter(ply, map_id)
 end
 
+-- called when a player removes their vote
+-- ply - the player who removes their vote
 function menu.OnVoterRemoved(ply)
 	panel:RemoveVoter(ply)
 end
 
+-- called after the vote finished
+-- map_id - the id of the map that won the vote
 function menu.OnWinnerAnnounced(map_id)
 	panel:AnnounceWinner(map_id)
 end
 
+-- called when the client uses the pam_toggle_menu command
 function menu.ToggleVisibility()
 	panel:SetVisible(not panel:IsVisible())
 end
 
+-- called when the player enables this menu via the menu manager
 function menu.OnEnable()
 
 end
 
+-- called when the player disables this menu via the menu manager
 function menu.OnDisable()
 
 end
 
+-- PAM_Register_Menus hook is used for registering this menu
 hook.Add("PAM_Register_Menus", "PAM_Register_Menus_Example", function()
 	PAM.RegisterMenu(menu)
 end)
--- wiki/pam_gui_example/lua/pam/client/cl_example_votescreen.lua
+++ wiki/pam_gui_example/lua/pam/client/cl_example_votescreen.lua
@@ -1,16 +1,16 @@
 PANEL = {}
 
-// Example Font
+-- Example Font
 surface.CreateFont("PAM_ExampleFont", {
 	font = "Trebuchet MS",
 	size = 20
 })
 
-// Example Text Color
-local exampleTextColor = {r = 255, g = 255, b = 255, a = 200}
+-- Example Text Color
+local example_text_color = {r = 255, g = 255, b = 255, a = 200}
 
 function PANEL:Init()
-	// Basic Settings
+	-- Basic Settings
 	local width = 500
 	local height = ScrH() * 0.75
 	self:SetSize(width, height)
@@ -19,113 +19,113 @@ function PANEL:Init()
 	self:SetTitle("Partly Adequate Mapvote GUI Example")
 	self:SetDeleteOnClose(false)
 
-	// stores all mapbuttons for easier access
-	self.MapButtons = {}
+	-- stores all mapbuttons for easier access
+	self.map_buttons = {}
 
-	// the countdown label
-	local LBLCountDown = vgui.Create("DLabel", self)
-	LBLCountDown:SetSize(width, 25)
-	LBLCountDown:Dock(TOP)
-	LBLCountDown:SetContentAlignment(5)
-	LBLCountDown:SetFont("PAM_ExampleFont")
-	// The Countdown updates itself every frame this way
-	LBLCountDown.Think = function()
-		// PAM.EndsAt stores the time the mapvote will end at
-		// This calculates the remaining time
-		local timeLeft = math.Round(math.max(PAM.EndsAt - CurTime(), 0))
-		LBLCountDown:SetText(timeLeft .. " seconds left!")
+	-- the countdown label
+	local lbl_countdown = vgui.Create("DLabel", self)
+	lbl_countdown:SetSize(width, 25)
+	lbl_countdown:Dock(TOP)
+	lbl_countdown:SetContentAlignment(5)
+	lbl_countdown:SetFont("PAM_ExampleFont")
+	-- The Countdown updates itself every frame this way
+	lbl_countdown.Think = function()
+		-- PAM.ends_at stores the time the mapvote will end at
+		-- This calculates the remaining time
+		local time_remaining = math.Round(math.max(PAM.ends_at - CurTime(), 0))
+		lbl_countdown:SetText(time_remaining .. " seconds left!")
 	end
 
-	// container for the maplist
-	local scrollPanel = vgui.Create("DScrollPanel", self)
-	scrollPanel:SetSize(width, height - 25)
-	scrollPanel:Dock(FILL)
-	// the maplist
-	local mapList = vgui.Create("DIconLayout", scrollPanel)
-	mapList:Dock(FILL)
+	-- container for the maplist
+	local sp_container = vgui.Create("DScrollPanel", self)
+	sp_container:SetSize(width, height - 25)
+	sp_container:Dock(FILL)
+	-- the maplist
+	local dil_map_list = vgui.Create("DIconLayout", sp_container)
+	dil_map_list:Dock(FILL)
 
-	// this creates one button for each map and adds them to self.MapButtons and the mapList
-	// mapinfo is a table containing the following values:
-	//   - id -> unique identifier
-	//   - name -> the map name
-	//   - playcount -> the amount of times this map has been played
-	for k, mapinfo in pairs(PAM.Maps) do
-		// creates a new button inside the maplist
-		local button = mapList:Add("DButton")
+	-- this creates one button for each map and adds them to self.MapButtons and the mapList
+	-- mapinfo is a table containing the following values:
+	--   - id -> unique identifier
+	--   - name -> the map name
+	--   - playcount -> the amount of times this map has been played
+	for k, mapinfo in pairs(PAM.maps) do
+		-- creates a new button inside the maplist
+		local button = dil_map_list:Add("DButton")
 
-		// caches the amount of voters on the assigned map
-		button.voterCount = 0
-		// the mapinfo
+		-- caches the amount of voters on the assigned map
+		button.voter_count = 0
+		-- the mapinfo
 		button.map = mapinfo
 
-		// basic settings
+		-- basic settings
 		button:SetSize(475, 25)
 		button:SetContentAlignment(4)
 		button:SetFont("PAM_ExampleFont")
-		button:SetTextColor(exampleTextColor)
+		button:SetTextColor(example_text_color)
 		button.Paint = function(s, w, h) end
 
-		// updates the button name every frame to show the current amount of voters
+		-- updates the button name every frame to show the current amount of voters
 		button.Think = function()
-			button:SetText(button.map.name .. " (played " .. button.map.playcount .. " times) [" .. tostring(button.voterCount) .. "]")
+			button:SetText(button.map.name .. " (played " .. button.map.playcount .. " times) [" .. tostring(button.voter_count) .. "]")
 		end
 
 		button.DoClick = function()
-			// This makes the local player vote for the assigned map
+			-- This makes the local player vote for the assigned map
 			net.Start("PAM_Vote")
 			net.WriteUInt(mapinfo.id, 32)
 			net.SendToServer()
 		end
 
-		// Adds this button to the MapButtons for better access
-		table.insert(self.MapButtons, button)
+		-- Adds this button to the MapButtons for better access
+		table.insert(self.map_buttons, button)
 	end
 
-	// get mouse focus
+	-- get mouse focus
 	self:MakePopup()
 	self:SetKeyboardInputEnabled(false)
 end
 
-// Updates all buttons votercounts when called
+-- Updates all buttons votercounts when called
 function PANEL:AddVoter(ply, id)
 	self:UpdateVoters()
 end
 
-// Updates all buttons votercounts when called
+-- Updates all buttons votercounts when called
 function PANEL:RemoveVoter(ply)
 	self:UpdateVoters()
 end
 
-// Updates all buttons votercounts when called
+-- Updates all buttons votercounts when called
 function PANEL:UpdateVoters()
-	// reset all votercounts
-	for _, mapButton in pairs(self.MapButtons) do
-		mapButton.voterCount = 0
+	-- reset all votercounts
+	for _, mapButton in pairs(self.map_buttons) do
+		mapButton.voter_count = 0
 	end
 
-	// add votercountsto the correct map buttons for each voter
-	for steamid, mapid in pairs(PAM.Votes) do
-		for _, mapButton in pairs(self.MapButtons) do
+	-- add votercountsto the correct map buttons for each voter
+	for steamid, mapid in pairs(PAM.votes) do
+		for _, mapButton in pairs(self.map_buttons) do
 			if(mapButton.map.id == mapid) then
-				mapButton.voterCount = mapButton.voterCount + 1
+				mapButton.voter_count = mapButton.voter_count + 1
 			end
 		end
 	end
 end
 
-// announces the winner
+-- announces the winner
 function PANEL:AnnounceWinner(id)
-	// shows the panel
+	-- shows the panel
 	self:SetVisible(true)
 
-	// deletes all buttons but the winner
-	for _, button in pairs(self.MapButtons) do
+	-- deletes all buttons but the winner
+	for _, button in pairs(self.map_buttons) do
 		if(button.map.id != id) then
 			button:Remove()
 		end
 	end
 
-	// plays a sound
+	-- plays a sound
 	timer.Create("ttt_pam_notification", 0.4, 3, function()
 		surface.PlaySound("hl1/fvox/blip.wav")
 	end)
