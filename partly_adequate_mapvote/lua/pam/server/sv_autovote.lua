336866686195dfaa96a0c88b4dabd8fa7cb5dc0d
Reispfannenfresser <Reispfannenfresser@no-reply.users.github.com>
2020-04-09 21:10:10 +0200

    TTT2LoadNextMap hook is now used

--- partly_adequate_mapvote/lua/pam/server/sv_autovote.lua
+++ partly_adequate_mapvote/lua/pam/server/sv_autovote.lua
@@ -1,20 +1,30 @@
---terrortown (regular ttt, ttt2 and tot)
---TODO use fancy hook once v0.6 of TTT2 is released
-hook.Add("Initialize", "PAM_TTT_AutoStart", function()
+hook.Add("Initialize", "PAM_Autostart", function()
+	-- terrortown support
 	if GAMEMODE_NAME == "terrortown" then
-		function CheckForMapSwitch()
-			local rounds_left = math.max(0, GetGlobalInt("ttt_rounds_left", 6) - 1)
+		if TTT2 then
+			-- TTT2 support
+			hook.Add("TTT2LoadNextMap", "PAM_Autostart_TTT2", function(nextmap, rounds_left, time_left)
+				PAM.Start(nil, false)
+				return true
+			end)
+		else
+			-- TTT and TOT support
+			function CheckForMapSwitch()
+				local rounds_left = math.max(0, GetGlobalInt("ttt_rounds_left", 6) - 1)
 
-			SetGlobalInt("ttt_rounds_left", rounds_left)
+				SetGlobalInt("ttt_rounds_left", rounds_left)
 
-			local time_left = math.max(0, (GetConVar("ttt_time_limit_minutes"):GetInt() * 60) - CurTime())
+				local time_left = math.max(0, (GetConVar("ttt_time_limit_minutes"):GetInt() * 60) - CurTime())
 
-			if rounds_left <= 0 or time_left <= 0 then
-				timer.Stop("end2prep")
-				if PAM.state == PAM.STATE_DISABLED then
-					PAM.Start(nil, false)
+				if rounds_left <= 0 or time_left <= 0 then
+					timer.Stop("end2prep")
+					if PAM.state == PAM.STATE_DISABLED then
+						PAM.Start(nil, false)
+					end
 				end
 			end
 		end
+		return
 	end
+	-- other gamemodes go here
 end)
