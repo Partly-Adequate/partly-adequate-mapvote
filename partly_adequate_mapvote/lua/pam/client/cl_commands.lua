36c54477210af61a785c22ab5c358eda0c871366
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2020-07-06 17:32:04 +0200

    The votescreen visibility can now be toggled when the vote is finished

--- partly_adequate_mapvote/lua/pam/client/cl_commands.lua
+++ partly_adequate_mapvote/lua/pam/client/cl_commands.lua
@@ -1,6 +1,6 @@
 --toggle menu visibility
 concommand.Add("pam_toggle_menu", function(player, cmd, args, arg_str)
-	if PAM.state == PAM.STATE_STARTED then
+	if PAM.state ~= PAM.STATE_DISABLED then
 		PAM.extension_handler.ToggleVisibility()
 	end
 end)
