aa32cf03ceb2a6f836ca630d1dca41b9c952ac5f
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2020-08-18 17:53:21 +0200

    added loader for special icons
    (the icons themselves are still missing)

---
+++ partly_adequate_mapvote/lua/pam/client/extensions/special_icons.lua
@@ -0,0 +1,15 @@
+local extension = {}
+extension.name = "special_icons"
+extension.enabled = true
+
+function extension.GetIconMaterial(option)
+	if not option.is_special then return end
+
+	local option_name = "img_" .. option.name
+
+    if file.Exists("materials/vgui/pam/" .. option_name .. ".vmt", "GAME") then
+        return Material("vgui/pam/" .. option_name .. ".vmt")
+    end
+end
+
+PAM.RegisterExtension(extension)
