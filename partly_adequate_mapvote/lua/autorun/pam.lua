692ae66798094c2815d59548a5226ecd61a1ff4d
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2020-08-19 04:45:42 +0200

    Added image for the random option

--- partly_adequate_mapvote/lua/autorun/pam.lua
+++ partly_adequate_mapvote/lua/autorun/pam.lua
@@ -18,6 +18,8 @@ if SERVER then
 
 	-- add resources
 	resource.AddFile("materials/vgui/pam/img_missing.vmt")
+	resource.AddFile("materials/vgui/pam/img_random_option.vmt")
+	resource.AddFile("materials/vgui/pam/img_change_gamemode.vmt")
 	resource.AddFile("materials/vgui/pam/ic_favorite.vmt")
 	resource.AddFile("materials/vgui/pam/ic_not_favorite.vmt")
 	resource.AddFile("materials/vgui/pam/ic_selected.vmt")
