214229ee70547e5ece42ac1068d126639f11387b
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2021-05-25 23:03:39 +0200

    added very simple permission check

--- partly_adequate_mapvote/lua/includes/modules/pacoman.lua
+++ partly_adequate_mapvote/lua/includes/modules/pacoman.lua
@@ -1391,7 +1391,8 @@ if SERVER then
 	-- @param ply the player who initiated the request
 	-- @local
 	local function ReceiveChangeRequest(len, ply)
-		-- TODO Permission checks
+		-- TODO more flexible permission checks
+		if not ply:IsSuperAdmin() then return end
 
 		local request_type = net.ReadUInt(3) + 1
 		local request_processor = request_processors[request_type]
