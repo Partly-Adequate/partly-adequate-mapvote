e0749e8735ed2b69bc61f0bd8f2afebb2d6cad0a
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-08 22:49:36 +0100

    Custom Round Counter Extension

---
+++ partly_adequate_mapvote/lua/pam/server/extensions/custom_round_counter.lua
@@ -0,0 +1,17 @@
+local name = "custom_round_counter"
+
+PAM_EXTENSION.name = name
+PAM_EXTENSION.enabled = false
+
+local setting_namespace = PAM.setting_namespace:AddChild(name)
+local round_limit = setting_namespace:AddSetting("round_limit", pacoman.TYPE_INTEGER, 6)
+
+local round_counter = 0;
+
+function PAM_EXTENSION:OnRoundEnded(self)
+	round_counter = round_counter + 1
+
+	if round_counter >= round_limit:GetActiveValue() then
+		PAM.Start()
+	end
+end
