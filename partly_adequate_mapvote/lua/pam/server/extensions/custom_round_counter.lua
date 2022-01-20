b83e614206fe2b2bdf420caa8f3f4bcac259a16e
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-20 22:03:05 +0100

    Made custom round counter variable modifiable

--- partly_adequate_mapvote/lua/pam/server/extensions/custom_round_counter.lua
+++ partly_adequate_mapvote/lua/pam/server/extensions/custom_round_counter.lua
@@ -6,12 +6,12 @@ PAM_EXTENSION.enabled = false
 local setting_namespace = PAM.setting_namespace:AddChild(name)
 local round_limit = setting_namespace:AddSetting("round_limit", pacoman.TYPE_INTEGER, 6)
 
-local round_counter = 0;
+PAM.custom_round_counter = 0;
 
 function PAM_EXTENSION:OnRoundEnded(self)
-	round_counter = round_counter + 1
+	PAM.custom_round_counter = PAM.custom_round_counter + 1
 
-	if round_counter >= round_limit:GetActiveValue() then
+	if PAM.custom_round_counter >= round_limit:GetActiveValue() then
 		PAM.Start()
 	end
 end
