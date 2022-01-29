ca3c54af7788275da1943ff4a05dff16ec026724
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-01-29 02:15:54 +0100

    Improved the custom round counter api.
    Any addon can now set and retrieve the round counter using pam events.

--- partly_adequate_mapvote/lua/pam/server/extensions/custom_round_counter.lua
+++ partly_adequate_mapvote/lua/pam/server/extensions/custom_round_counter.lua
@@ -6,12 +6,25 @@ PAM_EXTENSION.enabled = false
 local setting_namespace = PAM.setting_namespace:AddChild(name)
 local round_limit = setting_namespace:AddSetting("round_limit", pacoman.TYPE_INTEGER, 6)
 
-PAM.custom_round_counter = 0;
+local custom_round_counter = 0;
 
-function PAM_EXTENSION:OnRoundEnded(self)
-	PAM.custom_round_counter = PAM.custom_round_counter + 1
+function PAM_EXTENSION:OnRoundEnded()
+	PAM.extension_handler.RunEvent("SetRoundCounter", custom_round_counter + 1)
 
-	if PAM.custom_round_counter >= round_limit:GetActiveValue() then
+	if custom_round_counter >= round_limit:GetActiveValue() then
 		PAM.Start()
 	end
+end
+
+function PAM_EXTENSION:GetRoundCounter()
+	return custom_round_counter
+end
+
+function PAM_EXTENSION:SetRoundCounter(value)
+	if !isnumber(value) then
+		ErrorNoHaltWithStack("Expected a number, but was " .. type(value) .. "\n")
+		return
+	end
+
+	custom_round_counter = value
 end
